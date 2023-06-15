acb6835dddaeea64002579af525341e5d309cc86
Alpha <121796109+Alphastranger@users.noreply.github.com>
2023-06-15 15:55:37 -0400

    Styled the portfolio page

--- my-react-app/src/components/Project.js
+++ my-react-app/src/components/Project.js
@@ -11,29 +11,29 @@ const Project = (props)=>{
   minHeight:'150px',
   maxHeight: '200px',
   border: 'solid 2px black', minWidth:'150px', maxWidth:'300px'}}>
-                <a href='"https://kaylanorris.github.io/morning-java"'><h3 className="projTitles">Morning Java</h3></a>
-                <a href="https://github.com/KaylaNorris/morning-java"><i className="fa-brands fa-github projTitles"></i></a>
+                <a href='"https://kaylanorris.github.io/morning-java"'><h3 style={{display:'inline',position: 'relative', top: '170px', color: 'aliceblue', backgroundColor: 'rgb(39,114,107)', borderRadius: '3px'}}>Morning Java</h3></a>
+                <a href="https://github.com/KaylaNorris/morning-java"><i className="fa-brands fa-github" style={{display:'inline',position: 'relative', top: '170px', color: 'rgb(39,114,107)'}}></i></a>
             </div>
         <div  className={"project2"} style={{backgroundImage: `url(${Proj2})`,width:'100%', height:'100%', objectFit:'contain', backgroundSize: '100%',
   minHeight:'150px',
   maxHeight: '200px',
   border: 'solid 2px black', minWidth:'150px', maxWidth:'300px'}}>
-                <a href="https://gamereviewsnow.herokuapp.com/"><h3 className="projTitles">GameReviewsNow</h3></a>
-                <a href="https://github.com/Alphastranger/Project-2-Team-2"><i className="fa-brands fa-github projTitles"></i></a>
+                <a href="https://gamereviewsnow.herokuapp.com/"><h3 style={{display:'inline',position: 'relative', top: '170px', color: 'aliceblue', backgroundColor: 'rgb(39,114,107)', borderRadius: '3px'}}>GameReviewsNow</h3></a>
+                <a href="https://github.com/Alphastranger/Project-2-Team-2"><i className="fa-brands fa-github" style={{display:'inline',position: 'relative', top: '170px', color: 'rgb(39,114,107)'}}></i></a>
                 </div>
         <div  className={"project3"} style={{backgroundImage: `url(${Proj3})`, width:'100%', height:'100%', objectFit:'contain', backgroundSize: '100%',
   minHeight:'150px',
   maxHeight: '200px',
   border: 'solid 2px black', minWidth:'150px', maxWidth:'300px'}}>
-                <a href='https://alphastranger.github.io/Weather-Reporter/'><h3 className="projTitles">Weather Reporter</h3></a>
-                <a href="https://github.com/Alphastranger/Weather-Reporter"><i className="fa-brands fa-github projTitles"></i></a>
+                <a href='https://alphastranger.github.io/Weather-Reporter/'><h3 style={{display:'inline',position: 'relative', top: '170px', color: 'aliceblue', backgroundColor: 'rgb(39,114,107)', borderRadius: '3px'}}>Weather Reporter</h3></a>
+                <a href="https://github.com/Alphastranger/Weather-Reporter"><i className="fa-brands fa-github" style={{display:'inline',position: 'relative', top: '170px', color: 'rgb(39,114,107)'}}></i></a>
             </div>
         <div className={"project4"} style={{backgroundImage: `url(${Proj4})`, width:'100%', height:'100%', objectFit:'contain', backgroundSize: '100%',
   minHeight:'150px',
   maxHeight: '200px',
   border: 'solid 2px black', minWidth:'150px', maxWidth:'300px'}}>
-            <a href='https://alphastranger.github.io/Password-Generator/'><h3 className="projTitles">Password Generator</h3></a>
-            <a href="https://github.com/Alphastranger/Password-Generator"><i className="fa-brands fa-github projTitles"></i></a>
+            <a href='https://alphastranger.github.io/Password-Generator/'><h3 style={{display:'inline',position: 'relative', top: '170px', color: 'aliceblue', backgroundColor: 'rgb(39,114,107)', borderRadius: '3px'}}>Password Generator</h3></a>
+            <a href="https://github.com/Alphastranger/Password-Generator"><i className="fa-brands fa-github" style={{display:'inline',position: 'relative', top: '170px', color: 'rgb(39,114,107)'}}></i></a>
         </div>
         </div>
     )
