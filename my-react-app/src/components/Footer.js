6e075e24ff3dc620e738649d7f47abc7af62aa79
Alpha <121796109+Alphastranger@users.noreply.github.com>
2023-06-15 15:13:55 -0400

    Fixed some classes being called incorrectly and got some images to work. Still battling the CSS tho

--- my-react-app/src/components/Footer.js
+++ my-react-app/src/components/Footer.js
@@ -4,9 +4,9 @@ import Github from '../images/GithubLogo.png'
 const Footer = ()=>{
     return (
     <div className="bottomScreen">
-        <a href="https://github.com/Alphastranger" className={"footer"}><i class="fa-brands fa-github" alt="Github"></i></a>
-        <a href='https://www.linkedin.com/in/joseph-flynn-a024a21b2/'className={"footer"}><i class="fa-brands fa-linkedin" alt='LinkedIn'></i></a>
-        <a href='abjosammag@gmail.com' className={"footer"}><i class="fa-solid fa-envelope"></i></a>
+        <a href="https://github.com/Alphastranger" className={"footer"}><i className="fa-brands fa-github" alt="Github"></i></a>
+        <a href='https://www.linkedin.com/in/joseph-flynn-a024a21b2/'className={"footer"}><i className="fa-brands fa-linkedin" alt='LinkedIn'></i></a>
+        <a href='abjosammag@gmail.com' className={"footer"}><i className="fa-solid fa-envelope"></i></a>
     </div>
     )
 }
--- my-react-app/src/components/Project.js
+++ my-react-app/src/components/Project.js
@@ -1,33 +1,28 @@
 import React from "react";
 import Proj1 from '../images/Project1img.png'
+import Proj2 from '../images/GamesNow.png'
+import Proj3 from '../images/WeatherReporter.png'
+import Proj4 from '../images/PasswordGenerator.png'
 
 const Project = (props)=>{
     return (
         <div>
-        <a href='"https://kaylanorris.github.io/morning-java"' className={"project1"} style={{backgroundImage: `url(${Proj1})`}}>
-            <div>
-                <h3>Morning Java</h3>
-                <a href="https://github.com/KaylaNorris/morning-java"><i class="fa-brands fa-github"></i></a>
+        <div  className={"project1"} style={{backgroundImage: `url(${Proj1})`, width:'100%', height:'100%'}}>
+                <a href='"https://kaylanorris.github.io/morning-java"'><h3>Morning Java</h3></a>
+                <a href="https://github.com/KaylaNorris/morning-java"><i className="fa-brands fa-github"></i></a>
             </div>
-            </a>
-        <a href="https://gamereviewsnow.herokuapp.com/" className={"project2"}>
-            <div>
-                <h3>GameReviewsNow</h3>
-                <a href="https://github.com/Alphastranger/Project-2-Team-2"><i class="fa-brands fa-github"></i></a>
+        <div  className={"project2"} style={{backgroundImage: `url(${Proj2})`, width:'100%', height:'100%'}}>
+                <a href="https://gamereviewsnow.herokuapp.com/"><h3>GameReviewsNow</h3></a>
+                <a href="https://github.com/Alphastranger/Project-2-Team-2"><i className="fa-brands fa-github"></i></a>
                 </div>
-                </a>
-        <a href='https://alphastranger.github.io/Weather-Reporter/' className={"project3"}>
-            <div>
-                <h3>Weather Reporter</h3>
-                <a href="https://github.com/Alphastranger/Weather-Reporter"><i class="fa-brands fa-github"></i></a>
+        <div  className={"project3"} style={{backgroundImage: `url(${Proj3})`, width:'100%', height:'100%'}}>
+                <a href='https://alphastranger.github.io/Weather-Reporter/'><h3>Weather Reporter</h3></a>
+                <a href="https://github.com/Alphastranger/Weather-Reporter"><i className="fa-brands fa-github"></i></a>
             </div>
-            </a>
-        <a href='https://alphastranger.github.io/Password-Generator/' className={"project4"}>
-        <div>
-            <h3>Password Generator</h3>
-            <a href="https://github.com/Alphastranger/Password-Generator"><i class="fa-brands fa-github"></i></a>
+        <div className={"project4"} style={{backgroundImage: `url(${Proj4})`, width:'100%', height:'100%'}}>
+            <a href='https://alphastranger.github.io/Password-Generator/'><h3>Password Generator</h3></a>
+            <a href="https://github.com/Alphastranger/Password-Generator"><i className="fa-brands fa-github"></i></a>
         </div>
-        </a>
         </div>
     )
 }
