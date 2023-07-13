9d7856ba9d3d0ed2e7cea0cce664804337f90a2e
Alpha <121796109+Alphastranger@users.noreply.github.com>
2023-07-13 19:49:05 -0400

    Changed some more CSS

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -8,12 +8,16 @@ import Proj5 from '../images/Ecommerce.png'
 
 const Portfolio = () => {
     return (
+        <div>
+    <h2 className="portH2">Portfolio</h2>
         <div className={"portfolioPics"}>
+            
             <Project url="https://kaylanorris.github.io/morning-java" github="https://github.com/KaylaNorris/morning-java" image={Proj1} title='Morning Java'/>
             <Project url="https://gamereviewsnow.herokuapp.com/" github="https://github.com/Alphastranger/Project-2-Team-2" image={Proj2} title='GameReviewsNow'/>
             <Project url='https://alphastranger.github.io/Weather-Reporter/' github="https://github.com/Alphastranger/Weather-Reporter" image={Proj3} title='Weather Reporter'/>
             <Project url='https://alphastranger.github.io/Password-Generator/' github="https://github.com/Alphastranger/Password-Generator" image={Proj4} title='Password Generator'/>
             <Project url='https://lit-earth-19040-5018388296c4.herokuapp.com/ ' github="https://github.com/djamesclark/Coding-Library" image= {Proj5} title='Ecommerce Site'/>
+        </div>
         </div>
     )
 }
