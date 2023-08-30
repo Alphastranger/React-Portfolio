7ad80288366480949aa80c824ebe722631a1d451
Alpha <121796109+Alphastranger@users.noreply.github.com>
2023-08-29 21:11:57 -0400

    Fixed icon sizes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,9 +4,9 @@ import Github from '../images/GithubLogo.png'
 const Footer = ()=>{
     return (
     <div className="bottomScreen">
-        <a href="https://github.com/Alphastranger" className={"footer"}><i className="fa-brands fa-github" alt="Github"></i></a>
-        <a href='https://www.linkedin.com/in/joseph-flynn-a024a21b2/'className={"footer"}><i className="fa-brands fa-linkedin" alt='LinkedIn'></i></a>
-        <button onClick={()=>window.location.href = 'mailto:abjosammag@gmail.com'} className={"footer"}><i className="fa-solid fa-envelope"></i></button>
+        <a href="https://github.com/Alphastranger" className={"footer"}><i className="fa-brands fa-github fa-2x" alt="Github"></i></a>
+        <a href='https://www.linkedin.com/in/joseph-flynn-a024a21b2/'className={"footer"}><i className="fa-brands fa-linkedin fa-2x" alt='LinkedIn'></i></a>
+        <button onClick={()=>window.location.href = 'mailto:abjosammag@gmail.com'} className={"footer"}><i className="fa-solid fa-envelope fa-2x"></i></button>
     </div>
     )
 }
