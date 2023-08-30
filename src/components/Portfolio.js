94f4daa91fc811081375b74011d4b462124baf99
Alpha <121796109+Alphastranger@users.noreply.github.com>
2023-08-29 20:44:43 -0400

    Added portfolio descriptions

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -12,10 +12,10 @@ const Portfolio = () => {
     <h2 className="portH2">Portfolio</h2>
         <div className={"portfolioPics"}>
             <Project url="https://kaylanorris.github.io/morning-java" github="https://github.com/KaylaNorris/morning-java" image={Proj1} title='Morning Java' description='An all in one website that tracks news, the weather, and the stock market.'/>
-            <Project url="https://gamereviewsnow.herokuapp.com/" github="https://github.com/Alphastranger/Project-2-Team-2" image={Proj2} title='GameReviewsNow'/>
-            <Project url='https://alphastranger.github.io/Weather-Reporter/' github="https://github.com/Alphastranger/Weather-Reporter" image={Proj3} title='Weather Reporter'/>
-            <Project url='https://alphastranger.github.io/Password-Generator/' github="https://github.com/Alphastranger/Password-Generator" image={Proj4} title='Password Generator'/>
-            <Project url='https://lit-earth-19040-5018388296c4.herokuapp.com/ ' github="https://github.com/djamesclark/Coding-Library" image= {Proj5} title='Ecommerce Site'/>
+            <Project url="https://gamereviewsnow.herokuapp.com/" github="https://github.com/Alphastranger/Project-2-Team-2" image={Proj2} title='GameReviewsNow' description='Website for people to post reviews for upcoming and current video games.'/>
+            <Project url='https://alphastranger.github.io/Weather-Reporter/' github="https://github.com/Alphastranger/Weather-Reporter" image={Proj3} title='Weather Reporter' description='Website designed to track weather using an API.'/>
+            <Project url='https://alphastranger.github.io/Password-Generator/' github="https://github.com/Alphastranger/Password-Generator" image={Proj4} title='Password Generator' description='A random password generator website.'/>
+            <Project url='https://lit-earth-19040-5018388296c4.herokuapp.com/ ' github="https://github.com/djamesclark/Coding-Library" image= {Proj5} title='Ecommerce Site' description='An ecommerce website trading specifically in computer science texts.'/>
         </div>
         </div>
     )
